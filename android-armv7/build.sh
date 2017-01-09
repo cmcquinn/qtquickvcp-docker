4c08cc99aede45b280c2286482d3df70e125f3e4
Alexander Rössler <mail@roessler.systems>
2017-01-09 18:08:30 +0100

    adding local build scripts

---
+++ android-armv7/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker build -t --no-cache=true machinekoder/qtquickvcp:android-armv7 --rm=true .
---
+++ linux-x64/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker build --no-cache=true -t machinekoder/qtquickvcp:linux-x64 --rm=true .
+
