e50994d320111925082fc573dc7ebcc9b6e4f15b
Alexander Rössler <mail@roessler.systems>
2017-07-24 09:43:10 +0200

    update to Qt 5.9.1

--- linux-x64/init.sh
+++ linux-x64/init.sh
@@ -46,7 +46,7 @@ bash -ex build.sh
  cd ..
 
 # install Qt
-mkdir -p qt5 && wget -q -O qt5.tar.bz2 http://ci.roessler.systems/files/qt-bin/Qt-5.9-Linux-x64.tar.bz2
+mkdir -p qt5 && wget -q -O qt5.tar.bz2 http://ci.roessler.systems/files/qt-bin/Qt-5.9.1-Linux-x64.tar.bz2
 tar xjf qt5.tar.bz2 -C qt5
 rm qt5.tar.bz2
 
