8dc0e951c0f56f3e07d4a54955048d0c686ba555
Cameron McQuinn <cameron.mcquinn@gmail.com>
2017-11-22 22:12:01 -0800

    update Travis to use build script

--- linux-armhf/build.sh
+++ linux-armhf/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -t cmcquinn/qtquickvcp:linux-armhf . #--no-cache=true --rm=true .
+docker build -t cmcquinn/qtquickvcp-linux-armhf:latest . #--no-cache=true --rm=true .
